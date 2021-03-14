981221fac94775fdae4580c19084d7b9788c9371
eyalho <eyal.res.dev@gmail.com>
2021-03-14 18:03:18 +0200

    filter spans that contain invalid labels for NER (overlaping)

--- creating_data/A4_annotate_jsonl_based_on_patterns.py
+++ creating_data/A4_annotate_jsonl_based_on_patterns.py
@@ -4,8 +4,25 @@ import spacy
 import srsly
 from spacy.matcher import Matcher
 from spacy.tokens import Span
+from spacy.util import filter_spans
 
 from creating_data.config import PATTERNS_FILE_PATH, GIT_1_JSONL_PATH, GIT_1_ANNO_JSONL_PATH
+
+
+def _spans_to_spans_dicts_list(spans):
+    # print(span.label_, span.text, span.sent)
+    spans_dicts_list = []
+    for span in spans:
+        span_dict = dict()
+        span_dict["text"] = span.text  # same as line_nlp[start:end]
+        span_dict["start"] = span.start_char
+        span_dict["end"] = span.end_char
+        span_dict["label"] = span.label_
+        # span_dict["pattern"] = nlp.vocab.strings[match_id]  # Get string representation
+        span_dict["token_start"] = span.start
+        span_dict["token_end"] = span.end
+        spans_dicts_list.append(span_dict)
+    return spans_dicts_list
 
 
 def create_a_labeled_jsonl_dataset(pattern_file_path, jsonl_datafile, annotated_jsonl_datafile):
@@ -28,20 +45,13 @@ def create_a_labeled_jsonl_dataset(pattern_file_path, jsonl_datafile, annotated_
                 spans = []
                 for match_id, start, end in matches:
                     span = Span(line_nlp, start, end, label=match_id)
-                    print(idx, span)
-                    # print(span.label_, span.text, span.sent)
+                    spans.append(span)
+                spans = filter_spans(spans)
+                if spans:
+                    print(f"{idx}, spans({len(spans)}):{spans}")
+                spans_dicts_list = _spans_to_spans_dicts_list(spans)
 
-                    span_dict = dict()
-                    span_dict["text"] = span.text  # same as line_nlp[start:end]
-                    span_dict["start"] = span.start_char
-                    span_dict["end"] = span.end_char
-                    span_dict["label"] = span.label_
-                    # span_dict["pattern"] = nlp.vocab.strings[match_id]  # Get string representation
-                    span_dict["token_start"] = span.start
-                    span_dict["token_end"] = span.end
-                    spans.append(span_dict)
-                    # print(span_dict)
-                line_json["spans"] = spans
+                line_json["spans"] = spans_dicts_list
                 fw.write(json.dumps(line_json) + "\n")
 
 
